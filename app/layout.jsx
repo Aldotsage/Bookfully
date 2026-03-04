5f1bfb9293c98d7a03700957bece907d96cf679f
Aldotsage <thomasaldosalvia@gmail.com>
2026-03-04 11:40:46 +0100

    Fix sintassi JavaScript in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-﻿export default function RootLayout({ children }: { children: React.ReactNode }) {
+﻿export default function RootLayout({ children }) {
   return (
     <html lang="it">
       <body style={{ margin: 0, padding: 0 }}>{children}</body>
